4253f706cbda53f8a90dbf0d32bfbbfb98c731ca
mahakbabel12 <140581938+mahakbabel12@users.noreply.github.com>
2024-08-08 16:52:07 +0530

    Add files via upload

---
+++ odd_no.c
@@ -0,0 +1,37 @@
+// #include<stdio.h>
+// int main(){
+//     int a=1;
+//     while(a<=10){
+//         if(a%2!=0){
+//             printf("%d",a);
+//         }
+//         a++;
+
+//     }
+// }
+
+//1-->100 odd no
+// #include<stdio.h>
+// int main(){
+//     int a=1;
+//     while(a<=100){
+//         if(a%2!=0){
+//             printf("%d",a);
+//         }
+//         a++;
+
+//     }
+// }
+
+//45-->80 odd no
+#include<stdio.h>
+int main(){
+    int a=45;
+    while(a<=80){
+        if(a%2!=0){
+            printf("%d\n",a);
+        }
+        a++;
+    }
+    return 0;
+}
