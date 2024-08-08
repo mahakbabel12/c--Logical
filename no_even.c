b7784b7afd5139a9596913369a32efb2237df9a2
mahakbabel12 <140581938+mahakbabel12@users.noreply.github.com>
2024-08-08 16:39:41 +0530

    Add files via upload

---
+++ no_even.c
@@ -0,0 +1,36 @@
+//1-->5 even no.
+// #include<stdio.h>
+// int main(){
+//     int a=1;
+//     while(a<=5){
+//         if(a%2==0){
+//             printf("%d\n",a);
+//         }
+//         a++;
+//     }
+// }
+
+
+//---->10
+// #include<stdio.h>
+// int main(){
+//     int a=1;
+//     while(a<=10){
+//         if(a%2==0){
+//             printf("%d\n",a);
+//         }
+//         a++;
+//     }
+// }
+
+#include<stdio.h>
+int main(){
+    int a=1;
+    while(a<=100){
+        if(a%2==0){
+            printf("%d\n",a);
+        }
+        a++;
+    }
+}
+
