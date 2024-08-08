ae04097f6febb1ede9cd7784a609f04246fde5dd
mahakbabel12 <140581938+mahakbabel12@users.noreply.github.com>
2024-08-08 16:07:06 +0530

    Add files via upload

---
+++ no_reverse.c
@@ -0,0 +1,36 @@
+// #include<stdio.h>
+// int main(){
+//     int a=10;
+//     while(a>=1){
+//         printf("%d",a);
+//         a--;
+//     }
+// }
+
+
+// #include<stdio.h>
+// int main(){
+//     int a=89;
+//     while(a>=45){
+//         printf("%d",a);
+//         a--;
+//     }
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int a=100;
+//     while(a<=1){
+//         printf("%d",a);
+//         a--;
+//     }
+// }
+
+#include<stdio.h>
+int main(){
+    int a=100;
+    while(a>=50){
+        printf("%d",a);
+        a--;
+    }
+}
