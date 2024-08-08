c68e43b27394d03f3238cae4ad0d7deb44804d5b
mahakbabel12 <140581938+mahakbabel12@users.noreply.github.com>
2024-08-08 15:59:21 +0530

    Add files via upload

---
+++ while_no.c
@@ -0,0 +1,44 @@
+// #include<stdio.h>
+// int main(){
+//     int a=50;
+//     while(a<=100){
+//         printf("%d",a);
+//         a++;
+//     }
+//     return 0;
+// }
+
+
+// /*1-------->10*/
+// #include<stdio.h>
+// int main(){
+//     int a=1;
+//     while(a<=10){
+//         printf("%d\n",a);
+//         a++;
+//     }
+//     return 0;
+// }
+
+//1--->100
+// #include<stdio.h>
+// int main(){
+//     int a=1;
+//     while(a<=100){
+//         printf("%d",a);
+//         a++;
+//     }
+//     return 0;
+// }
+
+#include<stdio.h>
+int main(){
+    int a=45;
+    while(a<=89){
+        printf("%d",a);
+        a++;
+    }
+}
+
+
+
