2f45d9a4d2228d41092f4808533d698b92ac98f8
mahakbabel12 <140581938+mahakbabel12@users.noreply.github.com>
2024-08-08 12:36:23 +0530

    Add files via upload

---
+++ two.c
@@ -0,0 +1,19 @@
+//1)two number maximum 
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter a");
+    scanf("%d",&a);
+    printf("enter b");
+    scanf("%d",&b);
+    if(a>b)
+    {
+        printf("a is grater");
+    }
+    else
+    {
+        printf("b is greater");
+
+    }
+    return 0;
+}
