3f89d06474d47225ba0c39d9a0bbac17fb4aada8
mahakbabel12 <140581938+mahakbabel12@users.noreply.github.com>
2024-08-17 11:19:10 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,155 @@
+// *
+// **
+// ***
+// ****
+// *****
+
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=i;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// *****
+// ****
+// ***
+// **
+// *
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=i;j<=5;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+// *
+// **
+// ***
+// ****
+// *****
+// *****
+// ****
+// ***
+// **
+// *
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=i;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//         for(int i=1;i<=5;i++){
+//         for(int j=i;j<=5;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// *****
+// ****
+// ***
+// **
+// *
+// *
+// **
+// ***
+// ****
+// *****
+
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=i;j<=5;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//         for(int i=1;i<=5;i++){
+//         for(int j=1;j<=i;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+
+   
+// }
+
+
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
+
+// #include<stdio.h>
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=i;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//         for(int i=1;i<=4;i++){
+            
+//         for(int j=i;j<=4;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// *****
+// ****
+// ***
+// **
+// *
+// **
+// ***
+// ****
+// *****
+
+
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=4;i++){
+        for(int j=i;j<=5;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+        for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+
+   
+}
+
